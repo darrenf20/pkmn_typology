d1d716c733e27475f3193740c7546117ca521dcc
Darren Fielding <DVAXZ@proton.me>
2024-05-14 18:34:35 +0100

    Showing output for single types in attacking tab

---
+++ scripts/Gui.cs
@@ -0,0 +1,82 @@
+using Godot;
+using System;
+using System.Collections;
+using static TypeChart;
+
+public partial class Gui : Node
+{
+	private static ImageTexture[] icons = new ImageTexture[TypeChart.numTypes];
+	private static Hashtable outputTable = new Hashtable();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Get the nodes we need to prepare
+		OptionButton option = GetNode<OptionButton>("%TypeButton");
+
+		var output = GetNode<HFlowContainer>("%Output");
+		outputTable.Add(4.00f, output.GetNode("X4_1/Panel/List"));
+		outputTable.Add(2.00f, output.GetNode("X2_1/Panel/List"));
+		outputTable.Add(1.00f, output.GetNode("X1_1/Panel/List"));
+		outputTable.Add(0.50f, output.GetNode("X1_2/Panel/List"));
+		outputTable.Add(0.25f, output.GetNode("X1_4/Panel/List"));
+		outputTable.Add(0.00f, output.GetNode("X0_1/Panel/List"));
+
+		// Load the type icons
+		for (var i = 0; i < TypeChart.numTypes; i++)
+		{
+			// Create TextureImages for a type
+			string name = Enum.GetName(typeof(TypeChart.Type), i);
+			string path = "images/" + name.ToLower() + ".png";
+			var image = Image.LoadFromFile(path);
+			icons[i] = ImageTexture.CreateFromImage(image);
+
+			// Add it to type selectors
+			option.AddIconItem(icons[i], "");
+		}
+
+		// Remove radio buttons next to icons
+		var popup = option.GetPopup();
+		for (var i = 0; i < numTypes; i++)
+		{
+			popup.SetItemAsRadioCheckable(i, false);
+		}
+
+		// Fill in the type options
+		UpdateOutput();
+	}
+
+	private void UpdateOutput()
+	{
+		foreach (ItemList list in outputTable.Values)
+		{
+			list.Clear();
+		}
+
+		var selectedType = (TypeChart.Type)GetNode<OptionButton>("%TypeButton").GetSelectedId();
+
+		// Start with single types
+		foreach (var type in Enum.GetValues<TypeChart.Type>())
+		{
+			ImageTexture icon = icons[(int)type];
+			var modifier = TypeChart.GetModifier(selectedType, type);
+			var list = outputTable[modifier] as ItemList;
+			list.AddIconItem(icon, false);
+			list.AddIconItem(null, false); // Fill in empty slot of second column
+		}
+
+		// Show how many entries are in each list
+		foreach (ItemList list in outputTable.Values)
+		{
+			var count = list.ItemCount / 2;
+			list.GetNode<Label>("../../Head/Count").Text = "(" + count + ") ";
+		}
+	}
+
+	private void _on_type_button_item_selected(long index)
+	{
+		UpdateOutput();
+	}
+}
+
+
